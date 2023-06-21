2585f46f4412caa750631028ec680e8521566b10
Pit Steinbach <steinbach@pc.rwth-aachen.de>
2023-06-21 16:11:19 +0200

    linting and removing occu

--- test/validation_xtbml/test.py
+++ test/validation_xtbml/test.py
@@ -27,13 +27,13 @@ def test_dir(dir,ref_file,bin,args):
     tblite_xtbml_subprocess(dir,bin,args)
     df_test = pd.read_csv(dir+"/ml_feature_tblite.csv")
 
-    to_be_dropped_test = ['delta_qm_e_xx', 'q_d', 'qm_s_zz', 'chem.pot', 'qm_s_yz', 
+    to_be_dropped_test = ['delta_qm_e_xx', 'p_d', 'qm_s_zz', 'chem.pot', 'qm_s_yz', 
     'delta_qm_Z_yz', 'dipm_p', 'qm_s_xz', 'qm_s_yy', 'dipm_s_z', 'delta_dipm_Z', 'dipm_d', 
     'qm_s_xy', 'qm_d', 'delta_qm_e', 'delta_qm_e_xz', 'qm_d_zz', 'qm_d_xx',
     'dipm_d_z', 'dipm_d_y', 'qm_p_yz', 'qm_p',"delta_qm_e_yz",
     'qm_p_xz', 'dipm_A', 'dipm_s_x', 'dipm_p_x', 'qm_p_zz', 'response', 'qm_p_xy',
-    'q_p', 'qm_s', 'dipm_s_y', 'gap', 'qm_d_xy', 'dipm_d_x', 'qm_s_xx', 'delta_qm_e_zz',
-    'delta_dipm_A', 'qm_d_yz','qm_A', 'qm_d_yy', 'dipm_p_y', 'q_s', 'dipm_p_z', 'delta_qm_Z', 'qm_p_xx', 
+    'p_p', 'qm_s', 'dipm_s_y', 'gap', 'qm_d_xy', 'dipm_d_x', 'qm_s_xx', 'delta_qm_e_zz',
+    'delta_dipm_A', 'qm_d_yz','qm_A', 'qm_d_yy', 'dipm_p_y', 'p_s', 'dipm_p_z', 'delta_qm_Z', 'qm_p_xx', 
     'delta_qm_Z_zz', 'dipm_s', 'delta_qm', 'qm_d_xz', 'delta_qm_Z_xz', 'qm_p_yy', 'delta_dipm_e']
     df_test = df_test.drop(to_be_dropped_test,axis=1)
     df = abs(df_ref-df_test )
